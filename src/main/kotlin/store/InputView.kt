2997d3118539084d60b64ff176ae31822940bfe9
지원 <cheerfulboys@naver.com>
2024-11-06 04:12:59 +0900

    feat(InputView): 입력 안내 메세지와 입력을 반환하는 메서드들 추가

--- src/main/kotlin/store/InputView.kt
+++ src/main/kotlin/store/InputView.kt
@@ -1,4 +1,35 @@
 package store
 
+import camp.nextstep.edu.missionutils.Console
+
 class InputView {
+    fun readItem(): String {
+        println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])")
+        val input = Console.readLine()
+        return input
+    }
+
+    fun readBOGO(): String {
+        println("현재 {상품명}은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)")
+        val input = Console.readLine()
+        return input
+    }
+
+    fun readFullPrice(): String {
+        println("현재 {상품명} {수량}개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)")
+        val input = Console.readLine()
+        return input
+    }
+
+    fun readMembership(): String {
+        println("멤버십 할인을 받으시겠습니까? (Y/N)")
+        val input = Console.readLine()
+        return input
+    }
+
+    fun readAnythingElse(): String {
+        println("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)")
+        val input = Console.readLine()
+        return input
+    }
 }
