7bdc40252d7a7e25a08dbac185df55ba0037caec
지원 <cheerfulboys@naver.com>
2024-11-06 21:12:26 +0900

    feat(Membership): 멤버십 할인 금액 계산 메서드 추가 및 리드미 업데이트

--- src/main/kotlin/store/Membership.kt
+++ src/main/kotlin/store/Membership.kt
@@ -1,4 +1,15 @@
 package store
 
 class Membership {
+    fun getBenefitPrice(price: Int): Int {
+        val benefitPrice = price / 10 * 3
+        if (benefitPrice > MAX_BENEFIT_PRICE) {
+            return MAX_BENEFIT_PRICE
+        }
+        return benefitPrice
+    }
+
+    companion object {
+        const val MAX_BENEFIT_PRICE = 8_000
+    }
 }
