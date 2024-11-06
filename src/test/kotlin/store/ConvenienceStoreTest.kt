c9489f1450a6507a327a7fa3b158e4ea55636c98
지원 <cheerfulboys@naver.com>
2024-11-06 19:05:38 +0900

    test(ConvenienceStore): 입력 받은 상품명과 수량을 List<Cart>에 담아 반환하는 메서드 검증 추가

---
+++ src/test/kotlin/store/ConvenienceStoreTest.kt
@@ -0,0 +1,23 @@
+package store
+
+import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions.assertThat
+
+class ConvenienceStoreTest {
+    // addCart 메서드 테스트 작성
+    @Test
+    fun `입력을 장바구니에 추가하는 함수 검증`() {
+        // given
+        val input = "[사이다-2],[감자칩-1]"
+        val result: List<Cart> = listOf(
+            Cart("사이다", "2"),
+            Cart("감자칩", "1"),
+        )
+
+        // when
+        val carts: String = ConvenienceStore.addCart(input)
+
+        // then
+        assertThat(carts).isEqualTo(result)
+    }
+}
