2306520813192991e899e72f1b732d3ed23e234c
지원 <cheerfulboys@naver.com>
2024-11-06 23:39:38 +0900

    fix: 프로모션 재고만 있는 상품의 일반 재고 없음으로 출력하도록 수정함.

--- src/main/kotlin/store/ConvenienceStore.kt
+++ src/main/kotlin/store/ConvenienceStore.kt
@@ -8,12 +8,8 @@ class ConvenienceStore {
 
     private var products: MutableList<Product> = mutableListOf()
 
-    init {
-        outputView.printIntroduce()
-        readProductsFile()
-    }
-
     fun start() {
+        readProductsFile()
         outputView.printProducts(products)
         val items = inputView.readItem()
         addCart(items)
@@ -21,21 +17,43 @@ class ConvenienceStore {
 
     fun readProductsFile() {
         val path = "src/main/resources/products.md"
-        val productsFileContent = File(path).readLines().drop(1).dropLast(1)
-        productsFileContent.forEach { createProduct(it) }
+        val productsFileContent = File(path).readLines().drop(1)
+        var previousList: MutableMap<String, Int> = mutableMapOf()
+        var previous = ""
+        var productName = ""
+        productsFileContent.forEach {
+            val productInfo = it.split(",")
+            val name = productInfo[0]
+            if (previousList.containsKey(name)) {
+                previousList[name] = previousList[name]!! + 1
+            }
+            if (!previousList.containsKey(name)) {
+                previousList[name] = 1
+            }
+            if (!previous.contains(name) && previousList.containsKey(productName) && previous != "") {
+                val pre = previous.split(",")
+                if (previousList.getValue(productName) == 1 && pre[3] != "null") {
+                    createProduct(listOf(pre[0], pre[1], "재고 없음", "null"))
+                }
+            }
+            createProduct(productInfo)
+            previous = it
+            productName = productInfo[0]
+        }
     }
 
-    private fun createProduct(product: String) {
-        val productInfo = product.split(",")
+    private fun createProduct(productInfo: List<String>) {
         var promotion: String? = null
         if (productInfo[3] != "null") {
             promotion = productInfo[3]
         }
-        products.add(Product(
-            name = productInfo[0],
-            price = productInfo[1].toInt(),
-            quantity = productInfo[2],
-            promotion = promotion)
+        products.add(
+            Product(
+                name = productInfo[0],
+                price = productInfo[1].toInt(),
+                quantity = productInfo[2],
+                promotion = promotion,
+            )
         )
     }
 
@@ -43,8 +61,12 @@ class ConvenienceStore {
         val cartList: MutableList<Cart> = mutableListOf()
         items.split(",").forEach {
             val item = it.split("-")
-            cartList.add(Cart(item[0].replace("[", ""),
-                item[1].replace("]", "")))
+            cartList.add(
+                Cart(
+                    item[0].replace("[", ""),
+                    item[1].replace("]", "")
+                )
+            )
         }
         return cartList
     }
--- src/main/kotlin/store/OutputView.kt
+++ src/main/kotlin/store/OutputView.kt
@@ -5,17 +5,19 @@ import java.text.DecimalFormat
 class OutputView {
     val dec = DecimalFormat("#,###")
 
-    fun printIntroduce() {
-        println("안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.\n")
-    }
-
     fun printProducts(products: List<Product>) {
+        println("안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.\n")
         //TODO: 재고 파일에는 없어도, 프로모션 재고만 있는 상품에 대해 일반 상품의 재고 없음을 알려야 함.
         // "파일에 있는 상품 목록 출력" 테스트를 실행하였을 때
         // 현재 ["- 오렌지주스 1,800원 재고 없음", "- 탄산수 1,200원 재고 없음", "- 컵라면 1,700원 10개"] 누락됨.
 
         products.forEach {
-            var productInfo = "- ${it.name} ${dec.format(it.price)}원 ${it.quantity}개"
+            var productInfo = "- ${it.name} ${dec.format(it.price)}원"
+            if (it.quantity == "재고 없음") {
+                productInfo += " ${it.quantity}"
+            } else {
+                productInfo += " ${it.quantity}개"
+            }
             if (it.promotion != null) {
                 productInfo += " ${it.promotion}"
             }
