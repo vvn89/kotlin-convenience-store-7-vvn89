7be5a94ea708035b8ca4505c7ba340515417b0c9
지원 <cheerfulboys@naver.com>
2024-11-09 14:28:26 +0900

    test(Date): 구매 시점에 프로모션 기간 안에 포함되는지 검증하는 테스트 추가

--- src/test/kotlin/store/DateTest.kt
+++ src/test/kotlin/store/DateTest.kt
@@ -1,4 +1,19 @@
 package store
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
 class DateTest {
+    @Test
+    fun `프로모션 기간 안에 있는지 검증`() {
+        // given
+        val expectedResult = true
+        val input = "탄산2+1"
+
+        // when
+        val result = Date().isContainPromotion(input)
+
+        // then
+        assertThat(result).isEqualTo(expectedResult)
+    }
 }
